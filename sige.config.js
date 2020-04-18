f6de2c3dbe09c1e6b02dc08e4e43a6a6552ca076
Igor Ariza <igorariza@gmail.com>
2020-04-18 16:51:36 -0500

    rfactor: file

--- sige.config.js
+++ sige.config.js
@@ -1,12 +1,13 @@
-module.exports = { 
-  apps: [{ 
-    name: "sige", 
-    script: "./sigeClient/index.js", 
-    watch: true, 
-    env: { 
-      "NODE_ENV": "development", 
-    }, 
-    env_production: { 
-     " NODE_ENV ":" production " 
-    } 
-  },
+module.exports = {
+  apps: [{
+    name: "sige",
+    script: "./src/index.js",
+    watch: true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production: {
+      " NODE_ENV ": " production "
+    }
+  }]
+}
