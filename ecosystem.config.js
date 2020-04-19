14ff00c331d0634efe3566e369e51670ab984352
root <root@ip-172-31-82-153.ec2.internal>
2020-04-19 21:08:09 +0000

    deploy: version avaliable - ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,22 @@
 module.exports = {
-  apps: [
+  apps : [
     {
-      name: "sige-edu",
-      env: {
+     name: "sige-edu.com",
+     env: {
         PORT: 3000,
         NODE_ENV: "production"
       }
     }
-  ],
-  deploy: {
-    production: {
-      user: "ec2-user",
-      host: ["ec2-54-172-208-185.compute-1.amazonaws.com","54.172.208.185"],
+   ],
+  deploy : {
+    production : {
+      user : 'ec2-user',
+      host : ['ec2-54-172-208-185.compute-1.amazonaws.com','54.172.208.185'],
       key: "~/.ssh/sige.pem",
-      ref: "origin/master",
-      repo: "git@github.com:igorariza/sige-front.git",
-      path: "/var/www/sige-edu.com/",
-      "pre-setup" : "echo 'commands or local script path to be run on the host before the setup process starts'",
-      "post-setup": "echo 'commands or a script path to be run on the host after cloning the repo'",
+      ref  : 'origin/master',
+      repo : 'git@github.com:igorariza/sige-front.git',
+      path : '/var/www/sige-edu.com',
+      "pre-setup": "rm -rf /var/www/sige-edu.com/",
       "post-deploy":
         "npm install && npm run build && sudo cp -r build/* /var/www/sige-edu.com/"
     }
