f97bdcc1b5815212ebb6e2c19bbf33959eb0da20
Igor Ariza <igorariza@gmail.com>
2020-06-16 09:01:30 -0500

    feta.

--- src/components/university/Professorslist/ProfessorslistStaff.jsx
+++ src/components/university/Professorslist/ProfessorslistStaff.jsx
@@ -3,35 +3,27 @@ import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 import AvatarProfile from '../../common/Avatar/AvatarProfile'
 import AvatarProfileFemale from '../../common/Avatar/AvatarProfileFemale'
-import { Cardwrapperstaff, Imgteacherstaff,PaddingCard } from './stylesTeacherListStaff'
+// import { Cardwrapperstaff, Imgteacherstaff,PaddingCard } from './stylesTeacherListStaff'
 import SkeletonTeacherHome from '../../skeleton/SkeletonTeacherHome'
 import { CardTeacher } from 'components'
 var IMGDIR = process.env.REACT_APP_IMGDIR
 var BASEDIR = process.env.REACT_APP_BASEDIR
 
-
 const ProfessorslistStaff = (props) => {
-    if (props.loading) {
-        return <SkeletonTeacherHome />
-    }
-    return (
-        <div className="row">
-            {props.professors.map((teacher, key) => {
-                const nameTeacher = teacher.user.firstNameUser
-                return (
-                    <div className="col-md-6 col-lg-6" key={key}>
-                        
-                        <CardTeacher nameTeacher={nameTeacher}/>
-                        
-                    </div>
-                )
-            }
-            )
-
-            }
-
-        </div>
-
-    )
+  if (props.loading) {
+    return <SkeletonTeacherHome />
+  }
+  return (
+    <div className="row">
+      {props.professors.map((teacher, key) => {
+        const nameTeacher = teacher.user.firstNameUser
+        return (
+          <div className="col-md-6 col-lg-6" key={key}>
+            <CardTeacher nameTeacher={nameTeacher} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
-export default ProfessorslistStaff;
+export default ProfessorslistStaff
--- src/components/university/Professorslist/stylesTeacherListStaff.js
+++ src/components/university/Professorslist/stylesTeacherListStaff.js
@@ -1,7 +1,54 @@
-import styled, { keyframes } from 'styled-components'
-import { fadeIn } from '../../styles_animations/animation'
+// import styled, { keyframes } from 'styled-components'
+// import { fadeIn } from '../../styles_animations/animation'
 
+// export const Card = styled.div`
+//   width: 420px;
+//   height: 250px;
+//   background-color: #fff;
+//   background: linear-gradient(#92bca6, #a2ccb6);
+//   box-shadow: 0 8px 16px -8px rgba(0, 0, 0, 0.4);
+//   border-radius: 8px;
+//   overflow: hidden;
+//   position: relative;
+//   // padding: 0.125rem 0.75rem;
+// `
 
+// //${Imgteacherstaff};
+// export const Center = styled.div`
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   -webkit-transform: translate(-50%, -50%);
+// `
+// export const PaddingCard = styled.div`
+//   padding: 100px;
+// `
+// export const Pointscenter = styled.div`
+//   ${Center};
+//   top: 15%;
+//   color: #fff;
+//   text-transform: uppercase;
+//   font-size: 0.75em;
+//   font-weight: bold;
+//   background: rgba(0, 0, 0, 0.15);
+//   padding: 0.125rem 0.75rem;
+//   border-radius: 100px;
+//   white-space: nowrap;
+//   top: 85%;
+// `
+// export const Additional = styled.div`
+//   ${Card}:hover & {
+//     width: 100%;
+//     border-radius: 0 5px 5px 0;
+//   }
+//   position: absolute;
+//   width: 150px;
+//   height: 100%;
+//   transition: width 0.4s;
+//   overflow: hidden;
+//   z-index: 2;
+//   background: linear-gradient(#92bca6, #a2ccb6);
+// `
 export const Card = styled.div`
  
   width: 420px;
@@ -122,11 +169,78 @@ color: #fff;
 `;
 
 
+// export const Usercard = styled.div`
+//   width: 150px;
+//   height: 100%;
+//   position: relative;
+//   float: left;
+//   top: 15%;
+//   color: #fff;
+//   text-transform: uppercase;
+//   font-size: 0.75em;
+//   font-weight: bold;
+//   background: rgba(0, 0, 0, 0.15);
+//   border-radius: 100px;
+//   white-space: nowrap;
+//   &:after {
+//     content: '';
+//     display: block;
+//     position: absolute;
+//     top: 10%;
+//     right: -2px;
+//     height: 80%;
+//     border-left: 2px solid rgba(0, 0, 0, 0.025);
+//   }
+// `
+// export const Level = styled.div`
+//   ${Center};
+//   top: 15%;
+//   color: #fff;
+//   text-transform: uppercase;
+//   font-size: 0.75em;
+//   font-weight: bold;
+//   background: rgba(0, 0, 0, 0.15);
+//   padding: 0.125rem 0.75rem;
+//   border-radius: 100px;
+//   white-space: nowrap;
+// `
 
-export const Imgteacherstaff = styled.div`
-  ${fadeIn()}
-  padding: 0px; 
-`;
+// export const Moreinfo = styled.div`
+//   width: 300px;
+//   float: left;
+//   position: absolute;
+//   left: 150px;
+//   height: 100%;
+//   color: #fff;
+//   margin-bottom: 0;
+// `
+// export const Coords = styled.div`
+//   margin: 0 1rem;
+//   color: #fff;
+//   font-size: 1rem;
+//   float: right;
+// `
+// export const General = styled.div`
+//   width: 280px;
+//   height: 100%;
+//   position: absolute;
+//   top: 0;
+//   right: 0;
+//   z-index: 1;
+//   box-sizing: border-box;
+//   padding: 1rem;
+//   padding-top: 0;
+// `
+// export const Titlecard = styled.div`
+//   text-align: center;
+//   color: #fff;
+//   margin-bottom: 0;
+// `
+
+// export const Imgteacherstaff = styled.div`
+//   ${fadeIn()}
+//   padding: 0px;
+;`
 
 // export const inicioDiv = styled.div`
 //  display: flex;
